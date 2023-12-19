f30f87276bdc54558b03e6afc644d72064cfa924
DenysKharyna <deniskharyna@gmail.com>
2023-12-19 13:31:03 +0200

    Configured project

---
+++ MoviesListApp/Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MoviesListApp
+//
+//  Created by Денис Харына on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
